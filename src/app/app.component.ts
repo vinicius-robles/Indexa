340bb692ca54028071ce3fd6ae047f443640b637
vinicius-robles <vinicius.robles@gmail.com>
2024-12-19 16:30:31 -0300

    Adiciona uma API de contatos e implementa um método de filtragem dos contatos pela primeira letra do nome.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,15 @@ import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './componentes/container/container.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
+
+interface Contato {
+  id: number;
+  nome: string;
+  telefone: string;
+}
+
+import agenda from './agenda.json';
 
 @Component({
   selector: 'app-root',
@@ -14,14 +23,22 @@ import { SeparadorComponent } from './componentes/separador/separador.component'
     ContainerComponent,
     CabecalhoComponent,
     SeparadorComponent,
+    ContatoComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   alfabeto: string[] = [...'abcdefghijklmnopqrstuvwxyz'];
+  contatos: Contato[] = agenda;
 
   trackByFn(index: number, item: any): any {
     return item;
+  }
+
+  filtrarContatosPorLetraInicial(letra: string): Contato[] {
+    return this.contatos.filter((contato) => {
+      return contato.nome.toLowerCase().startsWith(letra);
+    });
   }
 }
